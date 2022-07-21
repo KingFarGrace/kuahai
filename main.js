c752d12cbc9ae2a6fb016e49eea0bfa587568518
KingFarGrace <KingFarGrace@163.com>
2022-07-21 12:27:18 +0800

    change vue framework to electron

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const window = new BrowserWindow( {
+        width : 800,
+        height : 600
+    })
+
+    window.loadFile("index.html")
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on("activate", () => {
+        if(BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on("window-all-closed", () => {
+    if(process.platform !== 'drawin') app.quit()
+})
